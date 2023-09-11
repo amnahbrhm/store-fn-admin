96406a82961fe31db9142f2d6ffafe00c3d0c509
Amnah <a.dugheshim@gmail.com>
2023-09-11 21:02:28 +0300

    add dist folder

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 const port = 3002
 
-app.use('/', express.static("dist/store"));
+app.use('/', express.static("dist"));
 
 app.listen(port, () => {
     console.log(`admin ${port}`)
