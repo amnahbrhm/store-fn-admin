a9107415f10df2f0712a22114201cd59393c7796
Amnah <a.dugheshim@gmail.com>
2023-09-13 20:30:12 +0300

    dist

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 const port = 3002
 
-app.use('/', express.static("dist"));
+app.use('/', express.static("dist/store-fn-admin"));
 
 app.listen(port, () => {
     console.log(`admin ${port}`)
