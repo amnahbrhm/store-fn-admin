87080bb3cb8b810ec7d91e04ca2a20ffe87ae2c9
Amnah <a.dugheshim@gmail.com>
2023-09-13 22:03:50 +0300

    dist

--- server.ts
+++ server.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from 'express';
+import path from 'path';
 const app: Express = express();
-const port = 3002
+const port = 3003
 
-app.use('/', express.static("dist/store-fn-admin"));
-app.use('/admin', express.static("dist/store-fn-admin"));
+// app.use('/', express.static("dist/store-fn-admin"));
+// app.use('/admin', express.static("dist/store-fn-admin"));
+app.use('/admin', express.static(path.join(__dirname, "dist")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`admin ${port}`)
 })
+
+
