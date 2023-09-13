ddfbd56971ac9de989ee6afd7a60f60d8637d301
Amnah <a.dugheshim@gmail.com>
2023-09-13 21:15:15 +0300

    server

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ const app: Express = express();
 const port = 3002
 
 app.use('/', express.static("dist/store-fn-admin"));
+app.use('/admin', express.static("dist/store-fn-admin"));
 
 app.listen(port, () => {
     console.log(`admin ${port}`)
