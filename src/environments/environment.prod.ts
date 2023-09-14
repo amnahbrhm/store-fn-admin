43a1ffe90a82e8813d17e64001c3d8f1bf93e2c7
Amnah <a.dugheshim@gmail.com>
2023-09-14 15:13:42 +0300

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    url: 'http://localhost:3000/api/'
+    url: 'http://13.51.171.52/'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    url: 'http://localhost:3000/api/'
+    url: 'http://13.51.171.52/'
 }
